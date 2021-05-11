c5343a3f92be8dc0a580be1f271acf7d7ef9fc2b
alishkeir <alishkeir_@hotmail.com>
2021-05-12 02:15:55 +0300

    Created Initial Test Sample

---
+++ jest.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from "@jest/types";
+
+const config: Config.InitialOptions = {
+  roots: ["<rootDir>/test", "<rootDir>/src"],
+  transform: {
+    //^  use 'ts-test' with tsx file
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
+
+  //^  take all files which have 'test' or 'spec' in their filename and treat them as a test file
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+
+  //^  what kind of files the test needs
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+
+  testEnvironment: "jsdom",
+  collectCoverage: true,
+  collectCoverageFrom: ["src/**/*.{ts,tsx}"],
+};
+
+export default config;
